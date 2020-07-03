bc059875de312e3d14eac742923e4aa44f71ec17
Manvikaul <47503962+Manvikaul@users.noreply.github.com>
2020-07-03 10:38:33 +0530

    day 22

---
+++ StringStream.cpp
@@ -0,0 +1,33 @@
+//hacerrank
+//c++
+
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+	// Complete this function
+    stringstream ss(str);
+    vector<int> ans;
+    char ch;
+    int num;
+
+    while(ss>>num)
+    {
+        ans.push_back(num);
+        ss>>ch;
+    }
+    return ans;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
